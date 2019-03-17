e452c18b429317b19c5fda9a64cc28bb25360c8b
cocoataster <cocoataster@protonmail.ch>
2019-03-17 19:44:45 +0100

    Add TableViewController wrapped in Nav + TabBar controllers. Override Data Source required methods

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Eric Sans Alvarez on 17/03/2019.
+//  Copyright © 2019 Eric Sans Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = ""
+        cell.detailTextLabel?.text = ""
+        return cell
+    }
+
+}
+
