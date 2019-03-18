b89804d8fdf1f33bd97499e2c715d6ff0ebec3ce
cocoataster <cocoataster@protonmail.ch>
2019-03-18 16:32:08 +0100

    Init Project 8

---
+++ Project8/Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Eric Sans Alvarez on 18/03/2019.
+//  Copyright © 2019 Eric Sans Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
